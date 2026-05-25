37a2e5050d0c11225f5475cf329c6312a8ecc275
Sandeep <sandeep.mamloda@vselindia.com>
2026-05-25 17:00:23 +0530

    changes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,31 +1,6 @@
-// import Footer from "@/components/common/footer/footer";
-// import Navbar from "@/components/common/navbar/navbar";
-// import CustomCursor from "./customcursor"; // ← ADD
-// import "./globals.css";
-
-// export const metadata = {
-//   title: "Portfolio",
-//   description: "Contact Me",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body>
-//         <CustomCursor /> {/* ← ADD */}
-//         <Navbar />
-//         {children}
-//         <Footer/>
-//       </body>
-//     </html>
-//   );
-// }
-
-// ========================================================
 import Footer from "@/components/common/footer/footer";
 import Navbar from "@/components/common/navbar/navbar";
-import CustomCursor from "./customcursor";
-import LenisProvider from "./lenisprovider";
+import CustomCursor from "./customcursor"; // ← ADD
 import "./globals.css";
 
 export const metadata = {
@@ -37,13 +12,38 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <LenisProvider>
-          <CustomCursor />
-          <Navbar />
-          {children}
-          <Footer />
-        </LenisProvider>
+        <CustomCursor /> {/* ← ADD */}
+        <Navbar />
+        {children}
+        <Footer/>
       </body>
     </html>
   );
 }
+
+// ========================================================
+// import Footer from "@/components/common/footer/footer";
+// import Navbar from "@/components/common/navbar/navbar";
+// import CustomCursor from "./customcursor";
+// import LenisProvider from "./lenisprovider";
+// import "./globals.css";
+
+// export const metadata = {
+//   title: "Portfolio",
+//   description: "Contact Me",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body>
+//         <LenisProvider>
+//           <CustomCursor />
+//           <Navbar />
+//           {children}
+//           <Footer />
+//         </LenisProvider>
+//       </body>
+//     </html>
+//   );
+// }
